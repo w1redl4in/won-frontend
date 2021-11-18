1bc19f49d1f650b0a480fb20ce704382fbd762f7
Felipe <felipe1180@hotmail.com>
2021-11-18 16:18:42 -0300

    test: :test_tube: testing wrapper backgroundColor

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -3,12 +3,16 @@ import { render, screen } from '@testing-library/react'
 import Main from '.'
 describe('<Main />', () => {
   it('should render the heading', () => {
-    const { container } = render(<Main />)
-
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the correct colors', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({
+      'background-color': '#06092b'
+    })
   })
 })
